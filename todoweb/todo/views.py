4fb19a673767b0cfa46c97c618ad16c168673cca
student-pro <student-pro@b301-2.khpk.ru>
2025-09-13 08:59:19 +0700

    feat: search

--- todoweb/todo/views.py
+++ todoweb/todo/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from .models import Task
+from django.db.models import Q
 
 # Create your views here.
 def index(request):
     tasks = Task.objects.all()
-    return render(request, 'index.html', { 'tasks' : tasks })
+
+    search = request.GET.get('search')
+    if search:
+        tasks = tasks.filter(Q(name__icontains = search) | Q(description__icontains = search))
+
+    context = {
+        'tasks': tasks,
+        'search': search,
+    }
+
+    return render(request, 'index.html', context)
